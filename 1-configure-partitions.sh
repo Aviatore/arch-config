b896b485564d6160b62d10f74d5293880bcd5835
Wojciech Wesołowski <wojtek.wes@gmail.com>
2025-07-13 18:30:01 +0200

    Add pause to the scripts

--- 1-configure-partitions.sh
+++ 1-configure-partitions.sh
@@ -20,42 +20,51 @@ sgdisk --new=5:0:0 $DEV
 sgdisk --typecode=1:8301 --typecode=2:ef02 --typecode=3:ef00 --typecode=5:8301 $DEV
 sgdisk --change-name=1:/boot --change-name=2:GRUB --change-name=3:EFI-SP --change-name=5:rootfs $DEV
 sgdisk --hybrid 1:2:3 $DEV
+read -p "Press ENTER to continue..." && echo ""
 
 echo ""
 
 echo "--- New partition structure ---"
 sgdisk --print $DEV
+read -p "Press ENTER to continue..." && echo ""
 
 echo ""
 
 echo "--- Encrypt /boot partition ---"
 cryptsetup luksFormat --type=luks1 ${DEVP}1
+read -p "Press ENTER to continue..." && echo ""
 
 echo ""
 
 echo "--- Encrypt operating system partition ---"
 cryptsetup luksFormat ${DEVP}5
+read -p "Press ENTER to continue..." && echo ""
 
 echo ""
 
 echo "--- Opening encrypted partitions ---"
 cryptsetup open ${DEVP}1 LUKS_BOOT
 cryptsetup open ${DEVP}5 ${DM}5_crypt
+read -p "Press ENTER to continue..." && echo ""
 
 echo "--- Formatting the boot partition ---"
 mkfs.ext4 -L boot /dev/mapper/LUKS_BOOT
+read -p "Press ENTER to continue..." && echo ""
 
 echo "--- Formatting the EFI partition ---"
 mkfs.vfat -F 16 -n EFI-SP ${DEVP}3
+read -p "Press ENTER to continue..." && echo ""
 
 echo "--- Configuring the LVM ---"
 pvcreate /dev/mapper/${DM}5_crypt
 vgcreate vgArch /dev/mapper/${DM}5_crypt
 lvcreate -L ${RAM}G -n swap_1 vgArch
 lvcreate -l 80%FREE -n root vgArch
+read -p "Press ENTER to continue..." && echo ""
 
 echo "--- Formatting the root partition ---"
 mkfs.ext4 /dev/mapper/vgArch-root
+read -p "Press ENTER to continue..." && echo ""
 
 # Install Arch
 echo "--- Initialize the swap partition ---"
@@ -65,9 +74,12 @@ echo "--- Mounting the partitions ---"
 mount /dev/mapper/vgArch-root /mnt
 mkdir /mnt/boot
 mount /dev/mapper/LUKS_BOOT /mnt/boot
+read -p "Press ENTER to continue..." && echo ""
 
 echo "--- Install essential packages ---"
 pacstrap -K /mnt base linux linux-firmware
+read -p "Press ENTER to continue..." && echo ""
 
 echo "--- Generate the fstab file ---"
 genfstab -U /mnt >> /mnt/etc/fstab
+read -p "Press ENTER to continue..." && echo ""
--- 2-run-inside-chroot.sh
+++ 2-run-inside-chroot.sh
@@ -3,12 +3,14 @@ echo "--- Configure time ---"
 ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
 hwclock --systohc # It will create the /etc/adjtime file
 tiomedatectl set-ntp true
+read -p "Press ENTER to continue..." && echo ""
 
 # Configure the locale
 echo "--- Configure the locale ---"
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
+read -p "Press ENTER to continue..." && echo ""
 
 # Network configuration
 echo "--- Network configuration ---"
@@ -20,6 +22,7 @@ pacman -S lvm2
 sed -i 's/HOOKS/#HOOKS/' /etc/mkinitcpio.conf
 echo "HOOKS=(base udev autodetect microcode modconf kms keyboard keymap consolefont block encrypt lvm2 filesystems fsck)" >> /etc/mkinitcpio.conf
 mkinitcpio -P
+read -p "Press ENTER to continue..." && echo ""
 
 # Set the root password
 echo "--- Set the root password ---"
@@ -28,6 +31,7 @@ passwd
 # Install the grub package
 echo "--- Install the grub package ---"
 pacman -S grub
+read -p "Press ENTER to continue..." && echo ""
 
 # Configure the boot loader
 echo "--- Configure the boot loader ---"
@@ -35,16 +39,20 @@ uuid=`blkid -s UUID -o value /dev/sda5`
 sed -i "s#GRUB_CMDLINE_LINUX=.*#GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=${uuid}:sda5_crypt root=/dev/mapper/vgArch-root\"#" /etc/default/grub
 sed -i "s#GRUB_PRELOAD_MODULES=.*#GRUB_PRELOAD_MODULES=\"part_gpt part_msdos lvm\"#" /etc/default/grub
 sed -i "s#GRUB_ENABLE_CRYPTODISK=.*#GRUB_ENABLE_CRYPTODISK=y#" /etc/default/grub
+read -p "Press ENTER to continue..." && echo ""
 
 # Update crypttab
 echo "--- Update crypttab ---"
 echo "LUKS_BOOT UUID=$(blkid -s UUID -o value /dev/sda1) none luks,discard" >> /etc/crypttab
 echo "sda5_crypt UUID=$(blkid -s UUID -o value /dev/sda5) none luks,discard" >> /etc/crypttab
+read -p "Press ENTER to continue..." && echo ""
 
 # Install grub
 echo "--- Install grub ---"
 grub-install /dev/sda
+read -p "Press ENTER to continue..." && echo ""
 
 # Create the grub config file
 echo "--- Create the grub config file ---"
 grub-mkconfig -o /boot/grub/grub.cfg
+read -p "Press ENTER to continue..." && echo ""
